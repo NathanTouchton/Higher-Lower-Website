a6eaf0a8da26cb1676ac37efffd3fb7bc4a68f59
Nathan Touchton <ntouchton@protonmail.com>
2023-05-18 23:54:14 -0500

    modified:   server.py 	new file:   templates/index.html

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 
 # Have user enter number between 0 and 9
@@ -11,5 +11,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def greeting():
+    """This is the homepage on the site that asks for a number."""
     return "<h1>Guess a number between 0 and 9: </h1>\
+        <input type=text inputmode=numeric>\
+            <input type=submit value=Submit>\
+            <br>\
         <img src=/static/fleventy-five.gif>"
+
+@app.route("/test-form")
+def form():
+    return render_template("index.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    input_value = request.form["input_field"]
+    return redirect("/result/" + input_value)
+
+@app.route("/result/<input_value>")
+def result(input_value):
+    return f"You typed {input_value}"
